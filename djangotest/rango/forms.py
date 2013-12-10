b5b1207ce9a94a9b940d10ac45f9da9c1186d249
neilfu <go2pkumba@gmail.com>
2013-12-10 15:30:49 +0800

    v0.1

---
+++ djangotest/rango/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="please engter the category name")
+    views = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+    
+    class Meta:
+        model = Category
+        
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128,help_text="please enter the page title")
+    url = forms.URLField(max_length=200, help_text="please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+    
+    class Meta:
+        model = Page
+        fields = ('title','url','views')
+        
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email','password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website','picture')
+    
